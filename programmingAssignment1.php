4269c88edfedfa652cb2fbbfea8b4e882dc600df
Julie Laursen <julielaursen1@gmail.com>
2019-12-14 20:04:17 -0600

    first assignment

---
+++ programmingAssignment1.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang=“en”>
+<head>
+<title>Sample PHP</title>
+</head>
+<body>
+<?php 
+$myName=Julie;
+$myAge = 21;
+$myCollege = ACC;
+echo "my name is" $myName;
+echo $myAge;
+echo $myCollege;
+?>
+</body>
+</html>
