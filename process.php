5261832b876b4fe398cc371eb4fed9e426c01f27
Julie Laursen <julielaursen1@gmail.com>
2019-12-17 22:15:04 -0600

    Draft of crud operations with a file in a textarea

--- process.php
+++ process.php
@@ -2,8 +2,31 @@
 
 <body>
 
-<?php echo $_POST['Name']; ?></br>
-<?php echo $_POST['Password']; ?> 
+<?php
+if(isset($_POST['submit']))
+{
+echo("Hello, " . $_POST['Name'] . ", your form has been submitted!<br>");
+}
+else
+{
+echo("Please make changes and submit the form.");
+}
+?>
+
+
+Name: <?php echo $_POST['Name']; ?></br>
+Password: <?php echo $_POST['Password']; ?> 
+<br>
+
+<?php
+$myFile = fopen("mycontent.txt", "w") or die("<br><strong>Unable to open file: </strong>" .$myFile);
+fwrite($myfile, $_POST['comments']);
+fclose($myFile);
+?>
+
+<?php
+echo readfile("mycontent.txt");
+?>
 
 </body>
 </html>
